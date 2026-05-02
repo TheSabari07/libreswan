6ee3e7cabaae99f14eed3d94f95d0df01ebe1910
Andrew Cagney <cagney@gnu.org>
2026-05-02 15:47:17 -0400

    testing: in check-06-dnssec update right.testing.libreswan.org
    ... as in change its value within DNS

--- testing/pluto/check-06-dnssec/all.nic-linuxeast-linuxwest.sh
+++ testing/pluto/check-06-dnssec/all.nic-linuxeast-linuxwest.sh
@@ -27,3 +27,24 @@ west# dig +short @192.1.2.254 -p 5353 east.testing.libreswan.org
 west# dig +short @192.1.2.254 -p 5353 chaos version.server txt
 west# echo done
 
+# now add some new records
+
+nic# echo 'right.testing.libreswan.org. IN A 192.1.2.23' >> /etc/nsd/zones/testing.libreswan.org # east
+nic# tail -1 /etc/nsd/zones/testing.libreswan.org
+nic# /testing/dnssec/sign-zone.sh /etc/nsd/zones/testing.libreswan.org
+nic# /testing/guestbin/nsd.sh reload
+nic# /testing/guestbin/unbound.sh reload
+
+west# dig +short @192.1.2.254 -p 5353 right.testing.libreswan.org # NSD
+west# dig +short @192.1.2.254         right.testing.libreswan.org # UNBOUND
+
+# never cross the stream!
+
+nic# sed -i -e '/right.testing.libreswan.org/ s/23/45/' /etc/nsd/zones/testing.libreswan.org # east->west
+nic# tail -1 /etc/nsd/zones/testing.libreswan.org
+nic# /testing/dnssec/sign-zone.sh /etc/nsd/zones/testing.libreswan.org
+nic# /testing/guestbin/nsd.sh reload
+nic# /testing/guestbin/unbound.sh reload
+
+west# dig +short @192.1.2.254 -p 5353 right.testing.libreswan.org # NSD
+west# dig +short @192.1.2.254         right.testing.libreswan.org # UNBOUND
