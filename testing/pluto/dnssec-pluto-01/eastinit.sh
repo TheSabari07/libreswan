1b8d1785c55d23dbca4365bfe03dc002f409e364
Andrew Cagney <cagney@gnu.org>
2026-05-03 10:11:23 -0400

    testing: drop dnssec-pluto-01
    Just an IKEv1 variation on ikev2-ddns-01-host-{ready,delayed}.

--- testing/pluto/dnssec-pluto-01/eastinit.sh
+++
@@ -1,8 +0,0 @@
-/testing/guestbin/swan-prep --hostkeys
-../../guestbin/mount-bind.sh /etc/hosts /etc/hosts
-ipsec start
-../../guestbin/wait-until-pluto-started
-echo 192.1.2.23 east-from-hosts-file.example.com east-from-hosts-file >> /etc/hosts
-ipsec auto --add westnet-eastnet
-ipsec auto --status
-echo "initdone"
